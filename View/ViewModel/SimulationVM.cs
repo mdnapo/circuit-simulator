d9ee41431b55dc879a38da03835253fa5b851754
mdnapo <md.napo@student.avans.nl>
2017-06-06 13:36:52 +0200

    Commit for unit testing

--- View/ViewModel/SimulationVM.cs
+++ View/ViewModel/SimulationVM.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core;
+using Core.Builders;
+using Core.Models.V2;
+using Parser;
 
 namespace View.ViewModel
 {
-	class SimulationVM
+	public class SimulationVM
 	{
+		public ObservableCollection<InputNode> Inputs { get; set; }
+		public ObservableCollection<InputNode> Outputs { get; set; }
+
+		private FileParser parser;
+		private CircuitBuilderV2 builder;
+		private Simulator simulator;
+
+		SimulationVM()
+		{
+			parser = new FileParser();
+			builder = new CircuitBuilderV2();
+			simulator = new Simulator();
+		}
+
+
 	}
 }
